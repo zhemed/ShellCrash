31956984f047d614f04cd3331345d3d9f1f3a861
zhemed <znhiiom@gmail.com>
2026-08-14 21:03:54 +0800

    内核下载自动规避 raw CDN 缓存：切换 jsDelivr 并同步安装包

--- scripts/libs/web_get_bin.sh
+++ scripts/libs/web_get_bin.sh
@@ -2,6 +2,8 @@
 
 get_bin() { #专用于项目内部文件的下载
     [ -z "$update_url" ] && update_url=https://testingcf.jsdelivr.net/gh/zhemed/ShellCrash@main
+    #raw.githubusercontent.com存在无法刷新的CDN缓存，自动切换至jsDelivr避免拉取到旧文件
+    [ -n "$update_url" ] && update_url=$(echo "$update_url" | sed 's#https://raw.githubusercontent.com/zhemed/ShellCrash/main#https://testingcf.jsdelivr.net/gh/zhemed/ShellCrash@main#')
     if [ -n "$url_id" ]; then
 		[ -n "$release_type" ] && rt="$release_type" || rt=master
         echo "$2" | grep -q '^bin/' && rt=update #/bin文件改为在update分支下载
